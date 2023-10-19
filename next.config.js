8f8581da99fa6b6dd5fc8515a7a3ba59c3ecf111
shirinox <cosminsebastian2004@gmail.com>
2023-10-20 01:31:53 +0300

    bug: vercel not adding blogs

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@ const withMDX = require("@next/mdx")()
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+    experimental: {
+        outputFileTracingIncludes: {
+            '/blogs': ['./blogs/*']
+        }
+    }
 }
 
 module.exports = withMDX(nextConfig)
